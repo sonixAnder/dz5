879178755624f98e6b9e07a71c81b504cf6ba31a
sonixAnder <148888208+sonixAnder@users.noreply.github.com>
2024-01-18 16:38:55 +0300

    Add files via upload

---
+++ дз1/дз1/дз1.cpp
@@ -0,0 +1,27 @@
+﻿#include "Header.h"
+
+int main() 
+{
+	setlocale(LC_ALL, "ru");
+	int inputSec, h, m, sec, h1, m1, sec1, temp;
+
+	cout << "Введите время в секундах: ";
+	cin >> inputSec;
+
+	h = inputSec / 3600;
+	temp = inputSec % 3600;
+	m = temp / 60;
+	sec = temp % 60;
+
+	cout << "Сейчас " << h << "ч" << ":" << m << "м" <<  ":" << sec << "сек" << "\n";
+	temp = 86400 - inputSec;
+	h1 = temp / 3600;
+	temp = temp % 3600;
+	m1 = temp / 60;
+	sec1 = temp % 60;
+
+	cout << "Осталось до полуночи: " << h1 << ":";
+	cout << m1 << ":" << sec1 << "\n";
+
+	return 0;
+}
---
+++ дз2/дз2/дз2.cpp
@@ -0,0 +1,17 @@
+﻿#include "Header.h"
+
+int main() 
+{
+    setlocale(LC_ALL, "ru");
+    int seconds;
+
+    cout << "Введите время в секундах: ";
+    cin >> seconds;
+
+    int remainingSeconds = 8 * 3600 - seconds;
+    int remainingHours = remainingSeconds / 3600;
+
+    cout << "Осталось впахивать " << remainingHours << " часов" << endl;
+
+    return 0;
+}
